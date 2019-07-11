211b603d4766d416fb50cfdcd9b44c76aafa77d1
satya190597 <webprakashsatya@gmail.com>
2019-07-11 18:28:47 +0530

    Two Way Data Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'todo-app';
+  todo:String = '';
   todoArray = [];
 
-  addTodo(value)
+  addTodo()
   {
-    this.todoArray.push(value);
-    console.log(value)
+    if(this.todo!=='' && this.todo.replace(' ','').length>0)
+    {
+      this.todoArray.push(this.todo);
+    }
+    this.todo = ''
   }
   delete(index)
   {
